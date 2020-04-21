1a9b82014dfa19cb336ac3d2b6fad210ad363166
Pantazis Theotokis <pan.theo@hotmail.com>
2020-04-21 17:37:32 +0100

    update colnames in download file only
    changed colnames in output df 
    Set colnames with a suffix:
    .query for the query column and .paralog for the paralogous columns.
    This will show only in the download file

--- paralog_app/global.R
+++ paralog_app/global.R
@@ -36,59 +36,30 @@ raw_data = dplyr::full_join(clinvar_P_LP,raw_data,by = c("var"))
 predict_output = function(input_data){
   print(input_data$mutation)
   print(raw_data$var[1])
-  # I opened formated and outputted again the RData odject from Nick to edit rownames and NAs 
-  # This can be done here within a function
-  # load the RData or rds file
-  # load("data/paralog_tmp.RData")
-  
-  #   exported RData with hardcoded dataframe name raw_data
-  # load("data/place_holder_results_datatable.RData",)
   
   #MOVED LOADING OF DATA TO ABOVE, OUTSIDE OF FUNCTION
 
-  # write new column chr:pos:ref:alt to look up
-  # raw_data$var<-paste(raw_data$CHROM.x,raw_data$POS.x,raw_data$REF.x,raw_data$ALT.x,sep="\t") #done above as website loads
-  
-  #generate Clinvar URLs for query and result
-  # moved that to the server side
-  #raw_data$link.x<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",raw_data$ID.x,"/"), "' target='_blank'>", raw_data$ID.x, "</a>")  
-  #raw_data$link.x<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",raw_data$ID.y,"/"), "' target='_blank'>", raw_data$ID.y, "</a>")  
-  
-  # select dataframe columns if not formated
-  # paralog_tmp<-subset(raw_data,select=c(raw_data$var,raw_data$CHROM.y ,raw_data$POS.y,raw_data$REF.y,raw_data$ALT.y,raw_data$ID.y ,raw_data$SYMBOL,raw_data$Protein_position.y,raw_data$REF_Amino_acids.y,raw_data$ALT_Amino_acids.y ,raw_data$Codons.y,raw_data$Para_Z_score.y))
-  # raw_data<-subset(raw_data,select=c(var, Gene, ID.x, CHROM.y ,POS.y,REF.y,ALT.y,ID.y ,SYMBOL,Protein_position.y,REF_Amino_acids.y,ALT_Amino_acids.y ,Codons.y,Para_Z_score.y),)
-  
-  # rename dataframe columns for webpage
-  # colnames(raw_data)<-c("Variant ID","Query_Gene","Query_ClinVar", "Chr","Position","REF","ALT","ClinVar_ID","Gene","Protein Position","Reference AA", "Alt AA","Codons","para_Z Score" )
-
   # select the vars ## this can be done first to reduce filtering time if final dataset is huge
   output = raw_data[raw_data$var %in%  input_data$mutation,]
-  # output = raw_data[raw_data$CHROM.x == as.numeric(input_data$chr) & 
-  #                     raw_data$POS.x == as.numeric(input_data$pos) & 
-  #                     raw_data$REF.x == input_data$ref &
-  #                     raw_data$ALT.x == input_data$alt,]
-  # output$CHROM.x = sapply(strsplit(output$var, "\t"), "[", 1)
-  # output$POS.x = sapply(strsplit(output$var, "\t"), "[", 2)
-  # output$REF.x = sapply(strsplit(output$var, "\t"), "[", 3)
-  # output$ALT.x = sapply(strsplit(output$var, "\t"), "[", 4)
-  output$ID<- paste0("<a href='", paste0("https://www.ncbi.nlm.nih.gov/clinvar/variation/",output$ID,"/"), "' target='_blank'>", output$ID, "</a>")
-  output = subset(output,select=c(
-    # CHROM.x, 
-    # POS.x, 
-    # REF.x, 
-    # ALT.x, 
-    var,
-    ID,
-    Gene, Codons.x, Protein_position.x, Amino_acids.x, Para_Z_score.x, var2, ID.y, SYMBOL, Codons.y, Protein_position.y, Amino_acids.y, Para_Z_score.y))
   
-  #in order to remove duplicated query rows
-  # tmp_chrom = NULL
-  # tmp_pos = NULL
-  # tmp_ref = NULL
-  # tmp_alt = NULL
-  # for (row in 1:nrow(output)){
-  #   
-  # }
+  # will change subset to dplyr::select to change colnames at the same time
+  # output = subset(output,select=c(
+  output = dplyr::select(output,
+    var.query=var,
+    ID.query=ID,
+    Gene.query=Gene, 
+    Codons.query=Codons.x, 
+    Protein_position.query=Protein_position.x, 
+    Amino_acids.query=Amino_acids.x, 
+    Para_Z_score.query=Para_Z_score.x, 
+    var.paralog=var2, 
+    ID.paralog=ID.y, 
+    SYMBOL.paralog=SYMBOL, 
+    Codons.paralog=Codons.y, 
+    Protein_position.paralog=Protein_position.y, 
+    Amino_acids.paralog=Amino_acids.y, 
+    Para_Z_score.paralog=Para_Z_score.y)
+  #)
   
   return(output)
   print(output)
